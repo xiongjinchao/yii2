842783b0d8096573ecf1ef813f7eef487b6d3106
xiongjinchao <xiongjinchao@baicheng.com>
2016-08-01 18:33:39 +0800

    'update'

--- backend/modules/user/models/UserSearch.php
+++ backend/modules/user/models/UserSearch.php
@@ -19,7 +19,7 @@ class UserSearch extends User
     {
         return [
             [['id', 'status', 'picture_id', 'created_at', 'updated_at'], 'integer'],
-            [['username', 'auth_key', 'password_hash', 'password_reset_token', 'email'], 'safe'],
+            [['username', 'mobile', 'auth_key', 'password_hash', 'password_reset_token', 'email'], 'safe'],
         ];
     }
 
