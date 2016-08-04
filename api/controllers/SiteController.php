89cbaa3bf16fe5b3e97893b8dfb44cb468ae93f4
xiongjinchao <xiongjinchao@baicheng.com>
2016-08-04 20:12:16 +0800

    api route

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -3,12 +3,15 @@ namespace api\controllers;
 
 use yii;
 use yii\web\Controller;
+use yii\helpers\Json;
+use yii\web\NotFoundHttpException;
 
 /**
  * Site controller
  */
 class SiteController extends Controller
 {
+    //http://api.yii2.com/?method=ranger.user.list&params[uid]=6218&params[page]=2&ip=192.168.1.1&timestamp=2015-06-21 17:18:09&key=9XNNXe66zOlSassjSKD5gry9BiN61IUEi8IpJmjBwvU07RXP0J3c4GnhZR3GKhMHa1A&sign=34A4C639B15718F3D8AF12EEE2B1BD47&device_id=100010&device_type=IOS&format=json&version=1.0
 
     /**
      * @inheritdoc
@@ -29,24 +32,58 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        //http://api.yii2.com/?method=ranger.user.get&params[uid]=6218&params[type]=H5&ip=192.168.1.1&timestamp=2015-06-21 17:18:09&key=9XNNXe66zOlSassjSKD5gry9BiN61IUEi8IpJmjBwvU07RXP0J3c4GnhZR3GKhMHa1A&sign=72624458773C56CDA8C5C2896F881011&chanel=IOS&format=json&version=1.0
-        $result = $this->runAction('api\modules\v1\TestAction',[]);
-        print_r($result);
+        $method = Yii::$app->request->get('method');
+        $method = explode('.',$method);
+        $version = explode('.',Yii::$app->request->get('version'))[0];
+        $params = Yii::$app->request->get('params');
+        $params = empty($params)?[]:$params;
+        $params['version'] = Yii::$app->request->get('version');
+        $params['device_type'] = Yii::$app->request->get('device_type');
+        $params['device_id'] = Yii::$app->request->get('device_id');
+        $params['timestamp'] = Yii::$app->request->get('timestamp');
+        $params['ip'] = Yii::$app->request->get('ip');
+
+        try {
+            $data = Yii::$app->runAction('/v'.$version.'/'.$method[1].'/'.$method[2],['params'=>$params]);
+            $result = [
+                'status' => 'success',
+                'code' => '0',
+                'data' => $data
+            ];
+        }catch (\Exception $e){
+            $result = [
+                'status' => 'success',
+                'code' => '0',
+                'data' => $e->getMessage()
+            ];
+        }
+        return $result;
     }
+
 
     public function beforeAction($action)
     {
-        echo 'before';
-        return parent::beforeAction($action);
+        $query = Yii::$app->request->get();
+        $secret = '27e1be4fdcaa83d7f61c489994ff6ed6';
+
+        $query['secret'] = $secret;
+        $sign = $query['sign'];
+        unset($query['sign']);
+
+        ksort($query['params']);
+        ksort($query);
+
+        $query = http_build_query($query);
+        if($sign === strtoupper(md5($query))) {
+            return parent::beforeAction($action);
+        }else{
+            throw new NotFoundHttpException();
+        }
     }
 
-    public function afterAction($action)
+    public function afterAction($action,$result)
     {
-        print_r($action);
+        echo Json::encode($result);
     }
 
-    public function actionTest()
-    {
-        echo 'test';
-    }
 }
--- api/modules/v1/controllers/UserController.php
+++ api/modules/v1/controllers/UserController.php
@@ -5,14 +5,15 @@ namespace api\modules\v1\controllers;
 use yii;
 use yii\web\Controller;
 use common\models\User;
-use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
 
 class UserController extends Controller
 {
-    public function actionList()
+    public function actionList($params)
     {
-        $result = User::find()->all();
-        echo 'ttttttt';
+        $result = array_map(function($record) {
+            return $record->attributes;
+        },User::find()->all());
         return $result;
     }
 
