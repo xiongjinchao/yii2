47be8080c57d2c1fd5891a5ada031a91b7de929e
xiongjinchao <xiongjinchao@hotmail.com>
2018-11-04 19:12:15 +0800

    language

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,6 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'layout' => '@app/views/layouts/adminlte/main',
+    'language' => 'zh-CN',
     'modules' => [
         'user' => [
             'class' => 'backend\modules\user\Module',
